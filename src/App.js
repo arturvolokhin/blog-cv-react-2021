7c7a161064477ef95b0e5b2ea128ae59c5a0d719
Arthur Volokhin <artur.volokhin@mail.ru>
2021-07-10 12:09:31 +0300

    feat: Add icon and text-content in Title.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Title from './Components/header/Title';
+import icon from './images/icons/icon.svg';
 import './style/main.css';
 
 const App = () => {
     return (
         <div className="wrapper">
-
+            <header className="header">
+                <Title icon={icon}/>
+            </header>
         </div>
     )
 }
---
+++ src/Components/header/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = ({icon}) => {
+    return(
+        <div className="title">
+            <img className="title__icon" src={icon} alt="icon" width="40px"></img>
+            <h1 className="title__text">Artur's V. Blog</h1>
+        </div>
+    )
+}
+
+export default Title;
