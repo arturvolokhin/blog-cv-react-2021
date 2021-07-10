e1e700b5febf5bacf87752ca6799657474b1e7c2
Arthur Volokhin <artur.volokhin@mail.ru>
2021-07-10 12:33:14 +0300

    fead: Add fonts and render ThemeSwitch.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Title from './Components/header/Title';
 import icon from './images/icons/icon.svg';
+import ThemeSwitch from './Components/header/ThemeSwitch'
 import './style/main.css';
+import './fonts/stylesheet.css';
 
 const App = () => {
     return (
         <div className="wrapper">
             <header className="header">
                 <Title icon={icon}/>
+                <ThemeSwitch/>
             </header>
         </div>
     )
---
+++ src/Components/header/ThemeSwitch.js
@@ -0,0 +1,10 @@
+const ThemeSwitch = () => {
+    return(
+        <label className="switch">
+            <input className="switch__checkbox" type="checkbox"/>
+            <span className="switch__slider"/>
+        </label>
+    )
+}
+
+export default ThemeSwitch;
